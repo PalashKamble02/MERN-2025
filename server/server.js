189a150f1495db1d61f6097368c54fc8a0927e47
palashkamble <kamblepalash356@gmail.com>
2025-02-27 10:47:48 +0530

    express 1.2

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
+const router = require("./router/auth-router")
 
-app.get('/',(req,res)=>{
-    res.status(200).send("Hellow World!")
-});
 
+
+//? Mount the Router: To use the router in your main Express app, you can "mount it at a specific URL prefix"
+app.use('/api/auth', router);
+
+// app.get('/',(req,res)=>{
+//     res.status(200).send("Hellow World!")
+// });
+
+// app.get('/second',(req,res)=>{
+//     res.status(200).send("Hello second")
+// });
 const PORT = 3000;
 
 app.listen(PORT,()=>{
