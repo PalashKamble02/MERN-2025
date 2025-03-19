0f55d473ee06b6b47bd1ee5e821732ca1314c899
palashkamble <kamblepalash356@gmail.com>
2025-03-19 15:28:18 +0530

    backend 1.9

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user-models");
-// const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs")
 //*-----------------------
 //* Controllers 
 //*----------------------
@@ -59,4 +59,34 @@ const register = async (req,res) =>{
     }
 }
 
-  module.exports= {home, register};
+
+ // *-------------------------
+  // * User Login Logic 
+  // *-------------------------
+const login = async(req, res)=>{
+  try {
+    const {email, password} = req.body;
+
+    const userExist = await User.findOne({email});
+
+    if(!userExist){
+      return res.status(400).json({message:"Invalid Credentials"});
+    }
+    const user = await bcrypt.compare(password, userExist.password);
+
+    if(user){
+      res.status(200).json({
+        msg:"Login Successful",
+        token:await userExist.generateToken(),
+        userId:userExist._id.toString(),
+      });
+    }else{
+      res.status(401).json({message:"Invalid email or password"})
+    }
+  } catch (error) {
+    res.status(500).json("Internal Server Error")
+  }
+}
+     
+
+  module.exports= {home, register, login};
